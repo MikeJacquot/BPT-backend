13553242847840337b495086d2a6794c2cd355ce
mickael@siliceum.com <mickael@siliceum.com>
2022-05-14 16:55:05 +0200

    [BPT-1] add family entity and relationships

---
+++ src/families/families.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUpdateFamilyDto } from './dto/create-family.dto';
+import { Family } from './entities/family.entity';
+
+@Injectable()
+export class FamiliesService {
+  
+  async create(createFamilyDto: CreateUpdateFamilyDto) {
+    const familyToCreate = Family.create(createFamilyDto);
+    return await familyToCreate.save();
+
+  }
+
+  async findAll() {
+    return await Family.find();
+  }
+
+  async findOne(id: string) {
+    return await Family.findOne(id);
+  }
+
+  async update(id: string, updateFamilyDto: CreateUpdateFamilyDto) {
+    return Family.update(id, updateFamilyDto);
+  }
+
+  async delete(id: string) {
+    return await Family.delete(id); 
+  }
+}
